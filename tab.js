e3cfd743d8a3d2180a5d779bf4a74edb9ae96dd3
sinsohi_04 <sohi8451@naver.com>
2024-01-26 13:51:32 +0900

    25강 - 이벤트 버블링과 이벤트관련 함수들

--- tab.js
+++ tab.js
@@ -1,7 +1,7 @@
 var btn = $(".tab-button");
 var content = $(".tab-content");
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 3; i++) {
   btn.eq(i).on("click", function () {
     btn.removeClass("orange");
     btn.eq(i).addClass("orange");
