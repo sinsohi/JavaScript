bc9c82d7d4fd46d0b4fd405ba90d6d8aa126d8da
sinsohi_04 <sohi8451@naver.com>
2024-01-26 14:36:07 +0900

    26강 - 이벤트 버블링과 이벤트관련 함수들

--- tab.js
+++ tab.js
@@ -1,11 +1,20 @@
 var btn = $(".tab-button");
 var content = $(".tab-content");
 
-for (let i = 0; i < 3; i++) {
-  btn.eq(i).on("click", function () {
-    btn.removeClass("orange");
-    btn.eq(i).addClass("orange");
-    content.removeClass("show");
-    content.eq(i).addClass("show");
-  });
+// for (let i = 0; i < 3; i++) {
+//   btn.eq(i).on("click", function () {
+//     OpenTab(i);
+//   });
+// }
+
+$(".list").click(function (e) {
+  console.log(e.target.dataset.id);
+  OpenTab(e.target.dataset.id);
+});
+
+function OpenTab(i) {
+  btn.removeClass("orange");
+  btn.eq(i).addClass("orange");
+  content.removeClass("show");
+  content.eq(i).addClass("show");
 }
