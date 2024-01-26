6fb57503a56790124db834369d74cda3977d4bca
sinsohi_04 <sohi8451@naver.com>
2024-01-26 13:25:53 +0900

    24강 - 탭기능 만들며 배우는 for 반복문 2

--- tab.js
+++ tab.js
@@ -1,62 +1,11 @@
-// 버튼0 누르면
-$(".tab-button")
-  .eq(0)
-  .on("click", function () {
-    // 모든 버튼에 붙은 orange 클래스명 제거
-    document.getElementsByClassName("tab-button")[0].classList.remove("orange");
-    document.getElementsByClassName("tab-button")[1].classList.remove("orange");
-    document.getElementsByClassName("tab-button")[2].classList.remove("orange");
+var btn = $(".tab-button");
+var content = $(".tab-content");
 
-    // 버튼0에 orange 클래스명 추가
-    document.getElementsByClassName("tab-button")[0].classList.add("orange");
-
-    // 모든 div에 붙은 show 클래스명 제거
-    document.getElementsByClassName("tab-content")[0].classList.remove("show");
-    document.getElementsByClassName("tab-content")[1].classList.remove("show");
-    document.getElementsByClassName("tab-content")[2].classList.remove("show");
-
-    // div0에 show 클래스명 추가
-    document.getElementsByClassName("tab-content")[0].classList.add("show");
+for (let i = 0; i < 5; i++) {
+  btn.eq(i).on("click", function () {
+    btn.removeClass("orange");
+    btn.eq(i).addClass("orange");
+    content.removeClass("show");
+    content.eq(i).addClass("show");
   });
-
-// 버튼1 누르면
-$(".tab-button")
-  .eq(1)
-  .on("click", function () {
-    // 모든 버튼에 붙은 orange 클래스명 제거
-    document.getElementsByClassName("tab-button")[0].classList.remove("orange");
-    document.getElementsByClassName("tab-button")[1].classList.remove("orange");
-    document.getElementsByClassName("tab-button")[2].classList.remove("orange");
-
-    // 버튼1에 orange 클래스명 추가
-    document.getElementsByClassName("tab-button")[1].classList.add("orange");
-
-    // 모든 div에 붙은 show 클래스명 제거
-    document.getElementsByClassName("tab-content")[0].classList.remove("show");
-    document.getElementsByClassName("tab-content")[1].classList.remove("show");
-    document.getElementsByClassName("tab-content")[2].classList.remove("show");
-
-    // div0에 show 클래스명 추가
-    document.getElementsByClassName("tab-content")[1].classList.add("show");
-  });
-
-// 버튼2 누르면
-$(".tab-button")
-  .eq(2)
-  .on("click", function () {
-    // 모든 버튼에 붙은 orange 클래스명 제거
-    document.getElementsByClassName("tab-button")[0].classList.remove("orange");
-    document.getElementsByClassName("tab-button")[1].classList.remove("orange");
-    document.getElementsByClassName("tab-button")[2].classList.remove("orange");
-
-    // 버튼0에 orange 클래스명 추가
-    document.getElementsByClassName("tab-button")[2].classList.add("orange");
-
-    // 모든 div에 붙은 show 클래스명 제거
-    document.getElementsByClassName("tab-content")[0].classList.remove("show");
-    document.getElementsByClassName("tab-content")[1].classList.remove("show");
-    document.getElementsByClassName("tab-content")[2].classList.remove("show");
-
-    // div0에 show 클래스명 추가
-    document.getElementsByClassName("tab-content")[2].classList.add("show");
-  });
+}
